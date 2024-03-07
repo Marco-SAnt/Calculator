7c22fe5e76b86e419cae20385c6e8d1fc1edf989
Marco-SAnt <marco.magalhaess.ant@gmail.com>
2024-03-07 14:45:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+const result = document.querySelector(".result");
+const buttons = document.querySelectorAll(".buttons button");
+let currentNumber = "";
+let firstOperand = null;
+let operator = null;
+let restart = false;
+
+function updateResult(originClear = false) {
+  result.innerText = originClear ? 0 : currentNumber.replace(".", ",");
+}
+
+function addDigit(digit) {
+  if (digit === "," && (currentNumber.includes(",") || !currentNumber)) return;
+  restart ? currentNumber = digit : currentNumber += digit;
+  restart = false;
+  updateResult();
+}
+
+function setOperator(newOperator) {
+  if (currentNumber) {
+    calculate();
+    firstOperand = parseFloat(currentNumber.replace(",", "."));
+    currentNumber = "";
+  }
+  operator = newOperator;
+}
+
+function calculate() {
+  if (operator === null || firstOperand === null) return;
+  let secondOperand = parseFloat(currentNumber.replace(",", "."));
+  let resultValue;
+  switch (operator) {
+    case "+": resultValue = firstOperand + secondOperand; break;
+    case "-": resultValue = firstOperand - secondOperand; break;
+    case "×": resultValue = firstOperand * secondOperand; break;
+    case "÷": resultValue = firstOperand / secondOperand; break;
+    default: return;
+  }
+  currentNumber = resultValue.toString().split(".")[1]?.length > 5 ? parseFloat(resultValue.toFixed(5)).toString() : resultValue.toString();
+  operator = null;
+  firstOperand = null;
+  restart = true;
+  updateResult();
+}
+
+function clearCalculator() {
+  currentNumber = "";
+  firstOperand = null;
+  operator = null;
+  updateResult(true);
+}
+
+function setPercentage() {
+  let result = parseFloat(currentNumber) / 100;
+  if (["+", "-"].includes(operator)) result *= firstOperand || 1;
+  currentNumber = result.toString().split(".")[1]?.length > 5 ? result.toFixed(5).toString() : result.toString();
+  updateResult();
+}
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const buttonText = button.innerText;
+    if (/^[0-9,]+$/.test(buttonText)) addDigit(buttonText);
+    else if (["+", "-", "×", "÷"].includes(buttonText)) setOperator(buttonText);
+    else if (buttonText === "=") calculate();
+    else if (buttonText === "C") clearCalculator();
+    else if (buttonText === "±") {
+      currentNumber = (parseFloat(currentNumber || firstOperand) * -1).toString();
+      updateResult();
+    }
+    else if (buttonText === "%") setPercentage();
+  });
+});
